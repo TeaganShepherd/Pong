615a1da21135a73e1ad373bb3f0f4d940c39b885
TeaganShepherd <teaganshepherd2@gmail.com>
2020-12-24 12:35:08 -0400

    Working on pong
    Styler and placed the sprite. Got ball moving. Need to move paddles and code the computer player's logic

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -10,11 +10,21 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball = SKSpriteNode()
+    var computer = SKSpriteNode()
+    var playerOne = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        computer = self.childNode(withName: "computer") as! SKSpriteNode
+        playerOne = self.childNode(withName: "playerOne") as! SKSpriteNode
         
+        ball.physicsBody?.applyImpulse(CGVector(dx: -15, dy: -15))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 0
+        self.physicsBody = border
     
     }
     
